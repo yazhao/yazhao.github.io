44206629ed07db5cfa498f6a437dd97b348f09e4
Alex Zhao <Alex Zhao>
2016-12-23 17:03:53 -0500

    URL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://yazhao.github.io'
+SITEURL = 'https://alexzhao.us'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
